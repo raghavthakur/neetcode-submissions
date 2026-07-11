0d03e20a81794c74e89b6ee5302e22ccc6967b1b
Raghav Thakur <raghavthakur.dev@gmail.com>
2026-07-10 17:09:22 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,60 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+        
+'''
+given two strings return true if two strings are anagrams otherwise return false
+
+exact same chars in two strings
+number of chars same in two strings
+orders can be different
+
+"racecar", "carrace" -> true
+
+"jar", "jam" -> false
+
+strings cannot be empty
+strings have only lowercase letters
+
+if two strings length is diff then return false
+char count of first string
+reduce char count from second string
+if char count is not empty or has some length then return false
+use dict
+add to dict by iterating first string
+remove to dict by iterating second string
+'''
+class Solution:
+    def isAnagram(self, string_one: str, string_two: str) -> bool:
+        # lengths diff
+        if len(string_one) != len(string_two):
+            return False
+
+        # iterate through string 1
+        char_count = {}
+        for char in string_one:
+            if char not in char_count:
+                char_count[char] = 0
+            char_count[char] += 1
+
+        # iterate through string 2
+        for char in string_two:
+            if char not in char_count:
+                return False
+            char_count[char] -= 1
+            if char_count[char] == 0:
+                del char_count[char]
+        
+        return len(char_count) == 0
+
+'''
+"racecar", "carrace" -> true
+                  i
+
+{} -> True
+
+runtime: O(n + m) where n is len of string 1 and m is len of string 2
+space: O(n)
+'''
+
+
+
